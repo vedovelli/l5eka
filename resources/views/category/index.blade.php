37eabc5968fc84ada8557b111ef4a172b297e86e
Vedovelli <vedovelli@gmail.com>
2015-05-30 08:19:44 -0300

    Inicio da aula 2. Pequenas mods no form categoria

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -6,44 +6,62 @@
     Categorias
 </h1>
 
-    <div class="well">
-        {!! Form::open() !!}
-            <div class="form-group">
-                {!! Form::label('name', 'Nome da Categoria', ['class' => 'control-label']) !!}
-                {!! Form::text('name', null, ['class' => 'form-control', 'id' => 'name']) !!}
-            </div>
-        {!! Form::close() !!}
-    </div>
-
-    <table class="table table-bordered table-striped table-hover" id="gridCategorias">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Nome</th>
-                <th>Criada em</th>
-                <th>Ações</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($categories as $cat)
-            <tr>
-                <td>{!! $cat['id'] !!}</td>
-                <td>{!! $cat['name'] !!}</td>
-                <td>{!! $cat['created_at'] !!}</td>
-                <td>
-                    <button class="btn btn-xs btn-primary">editar</button>
-                    <button class="btn btn-xs btn-danger">excluir</button>
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-
-    <p>Página {!! $categories->currentPage() !!} de {!! $categories->lastPage() !!}</p>
-
     <div class="row">
-        <div class="col-md-12 text-center">
-            {!! $categories->render() !!}
+        <div class="col-md-6">
+
+            {{-- Lista --}}
+            <table class="table table-bordered table-striped table-hover" id="gridCategorias">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>Criada em</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($categories as $cat)
+                    <tr>
+                        <td>{!! $cat['id'] !!}</td>
+                        <td>{!! $cat['name'] !!}</td>
+                        <td>{!! $cat['created_at'] !!}</td>
+                        <td>
+                            <button class="btn btn-xs btn-primary">editar</button>
+                            <button class="btn btn-xs btn-danger">excluir</button>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+
+            {{-- Paginacao --}}
+            <p>Página {!! $categories->currentPage() !!} de {!! $categories->lastPage() !!}</p>
+
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    {!! $categories->render() !!}
+                </div>
+            </div>
+            {{-- Paginacao End --}}
+
+        </div>
+        <div class="col-md-6">
+            <div class="well">
+                {!! Form::open() !!}
+                    <div class="form-group">
+                        {!! Form::label('name', 'Nome da Categoria', ['class' => 'control-label']) !!}
+                        {!! Form::text('name', null, ['class' => 'form-control', 'id' => 'name']) !!}
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12 text-right">
+                            <button class="btn btn-primary" type="submit">
+                                <i class="fa fa-fw fa-save"></i>
+                                Salvar
+                            </button>
+                        </div>
+                    </div>
+                {!! Form::close() !!}
+            </div>
         </div>
     </div>
 
